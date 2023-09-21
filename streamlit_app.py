3ed2015cf4723e6dbe904a80795016873020508e
itou-daiki <141300506+itou-daiki@users.noreply.github.com>
2023-09-21 05:41:04 +0000

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,67 +1,37 @@
-# Streamlitライブラリをインポート
-import streamlit as st
-
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
-# CSVファイルから質問と選択肢を読み込む
+# Excelファイルを読み込む
 @st.cache
-def load_data(filename):
-    data = pd.read_csv(filename)
+def load_data():
+    # Excelファイルのパスを指定します
+    path = "your_github_repo_path/questionnaire.xlsx"
+    data = pd.read_excel(path)
     return data
 
-filename = 'questions.csv'
-data = load_data(filename)
+data = load_data()
 
-# Streamlitアプリの設定
-st.title("アンケートアプリ")
-st.write("以下のアンケートにお答えいただき、ポイントを計算します。")
+# アンケートの回答を収集
+responses = {}
+for index, row in data.iterrows():
+    question_number = row["設問番号"]
+    question_name = row["設問名"]
+    answer = st.radio(
+        f"{question_number}. {question_name}",
+        ["4　とてもあてはまる","3　少しあてはまる","2　あまりあてはまらない","1　全くあてはまらない"]
+    )
+    responses[question_number] = {"answer": int(answer[0]), "factor": row["因子名"]}
 
-# ユーザーからのアンケート回答を収集
-user_points = {}
-for _, row in data.iterrows():
-    question = row['question']
-    choices = [row['choice1'], row['choice2'], row['choice3'], row['choice4']]
-    answer = st.radio(question, choices)
-    user_points[question] = answer
+# 平均点の計算
+factors = data["因子名"].unique()
+avg_scores = {}
+for factor in factors:
+    total = sum([resp["answer"] for q_num, resp in responses.items() if resp["factor"] == factor])
+    count = sum([1 for q_num, resp in responses.items() if resp["factor"] == factor])
+    avg_scores[factor] = total / count
 
-# ポイントを計算
-points_per_question = {
-    "質問1: あなたの年齢は？": {
-        "18歳未満": 0,
-        "18歳から30歳まで": 2,
-        "31歳から50歳まで": 5,
-        "51歳以上": 10,
-    },
-    "質問2: 好きな色は何ですか？": {
-        "青": 3,
-        "赤": 2,
-        "緑": 1,
-        "その他": 0,
-    },
-    "質問3: どのプログラミング言語を使用しますか？": {
-        "Python": 5,
-        "JavaScript": 3,
-        "Java": 2,
-        "その他": 1,
-    },
-}
-total_points = sum(points_per_question[question][user_points[question]] for question in data['question'])
-
-# 結果を表示
-st.write(f"獲得したポイント: {total_points} ポイント")
-
-# ポイントに応じた可視化（例：棒グラフ）
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-sns.set(style="whitegrid")
-plt.figure(figsize=(10, 6))
-ax = sns.barplot(x=list(user_points.keys()), y=[points_per_question[question][user_points[question]] for question in data['question']])
-plt.xticks(rotation=45)
-plt.xlabel("質問")
-plt.ylabel("ポイント")
-plt.title("質問ごとのポイント")
-st.pyplot(plt)
-
-
+# レーダーチャートの表示
+df_avg_scores = pd.DataFrame([avg_scores])
+fig = px.line_polar(df_avg_scores, r=df_avg_scores.columns, theta=df_avg_scores.columns, line_close=True)
+st.plotly_chart(fig)
