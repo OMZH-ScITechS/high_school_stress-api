5d03a309ecfbebdeec97367b6db834c4b8394d40
29Ssugerloaf18 <144981236+29Ssugerloaf18@users.noreply.github.com>
2023-12-06 08:23:50 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,19 +68,25 @@ if factor_scores:
 
 st.subheader("＜F1　人間関係＞")
 if avg_score_f1 < 2.5 :
+    #平均値未満の時のメッセージ
     st.write("aaa")
 else:
+    #平均値以上の時のメッセージ
     st.write("ddd")
 
 st.subheader("＜F2　心理的余裕＞")
 if avg_score_f2 < 2.5 :
+    #平均値未満の時のメッセージ
     st.write("bbb")
 else:
+    #平均値以上の時のメッセージ
     st.write("eee")
 
 st.subheader("＜F3　食事・睡眠＞")
 if avg_score_f3 < 2.5 :
+    #平均値未満の時のメッセージ
     st.write("CCC")
 else:
+    #平均値以上の時のメッセージ
     st.write("fff")
 
