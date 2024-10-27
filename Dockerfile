548a5fc57ad4d59cf75b50db91307efb99aab5a0
launchpencil <launchpencil@gmail.com>
2024-10-27 14:20:15 +0900

    できた

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.9-slim
+
+WORKDIR /app
+
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    curl \
+    software-properties-common \
+    git \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY app.py /app/
+COPY f1.xlsx /app/
+COPY newmodel.pkl /app/
+COPY requirements.txt /app/
+
+RUN pip3 install -r requirements.txt
+
+EXPOSE 5000
+
+HEALTHCHECK CMD curl --fail http://localhost:5000/_stcore/health
+
+ENTRYPOINT ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import pickle
+from flask import Flask, request, jsonify
+
+results = []
+model = pickle.load(open('newmodel.pkl', 'rb'))
+app = Flask(__name__)
+
+@app.route('/stress/predict', methods=['POST'])
+def handle_json():
+    try:
+        if not request.is_json:
+            return jsonify({"error": "Content-Type must be application/json"}), 400
+
+        results = request.get_json()['list']
+        results_array = np.array(results).reshape(1, -1)
+        prediction = model.predict(results_array)
+
+        # 応答データの作成
+        response_data = {
+            "received_data": request.get_json()['list'],
+            "prediction" : prediction[0]
+        }
+
+        # JSON形式で応答を返す
+        return jsonify(response_data), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# エラーハンドリング
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({"error": "Bad Request"}), 400
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return jsonify({"error": "Method Not Allowed"}), 405
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+    print('server stated')
